ef3c6741896cabc68152d18dbe168e8b9d2fde81
chrisk <compbio@mole-dhcp-203.baker>
2015-10-23 17:32:05 -0700

    r_prog HW2 for indigo c. king

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+# These functions can be used to cache matrix inverse operations
 
-## Write a short comment describing this function
-
+## makeCacheMatrix creates object that caches matrix and inverse and calcs if doesnt exist
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function() x
+	setinv <- function( solve ) inv <<- solve
+	getinv <- function() inv
+	list(set = set, get = get, setinv = setinv, getinv = getinv)
+		
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve asks for inverse val of above object if exists or asks makeCacheMatrix to solve it
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	## Return a matrix that is the inverse of 'x'
+	inv <- x$getinv()
+	if(!is.null(inv)) {
+		message("getting cached data")
+		return(inv)
+	}
+	data <- x$get()
+	inv <- solve(data, ...)
+	x$setinv(inv)
+	inv
+
 }
